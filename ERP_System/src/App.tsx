32c6ae43ce18b48952e7fa9b4624048176ae706e
Safarova1 <samasafarova1998@gmail.com>
2024-08-14 16:49:12 +0400

    initial dashboard setup

--- ERP_System/src/App.tsx
+++ ERP_System/src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Dashboard from './pages/Dashboard/Dashboard'
 
 function App() {
 
   return (
     <>
-      <h1>Welcome to our project!</h1>
+      <Dashboard/>
     </>
   )
 }
